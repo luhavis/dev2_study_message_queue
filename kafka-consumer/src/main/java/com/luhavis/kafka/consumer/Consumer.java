64dfd3280f2dba21206d740e7bccb88afac06b41
luhavis <luhavisdev@gmail.com>
2021-02-12 23:30:14 +0900

    init

---
+++ kafka-consumer/src/main/java/com/luhavis/kafka/consumer/Consumer.java
@@ -0,0 +1,38 @@
+package com.luhavis.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Consumer {
+    private static String TOPIC_NAME = "test";
+    private static String GROUP_ID = "testgroup";
+    private static String BOOTSTRAP_SERVERS = "172.22.87.232:29092";
+
+    public static void main () {
+        Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(configs);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME)); // ?
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("=========== Get record key and value ===========");
+                System.out.println("key : " + record.key() + " | value : " + record.value());
+            }
+        }
+
+    }
+
+}
---
+++ kafka-producer/src/main/java/com/luhavis/kafka/producer/Producer.java
@@ -0,0 +1,42 @@
+package com.luhavis.kafka.producer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Producer {
+    private static String TOPIC_NAME = "test";
+    private static String GROUP_ID = "testgroup";
+
+    /** Broker의 호스트 IP 정보
+     *  여러 Broker의 호스트 IP 정보 설정 시 콤마(,)로 구분
+     *  예) 192.168.0.1:9092,192.168.0.2:9092
+     *  */
+    private static String BOOTSTRAP_SERVERS = "172.22.87.232:29092";
+
+    public static void main () {
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(configs);
+
+
+        for (int i=0; i<10; i++) {
+            String data = "data " + i;
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, Integer.toString(i), data);
+            try {
+                producer.send(record);
+                System.out.println("Send to " + TOPIC_NAME + " | data : " + data);
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
